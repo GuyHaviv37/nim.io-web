f4b85ee25dcb1cdb20dbd2b67c9739b1eacfdd21
GuyHaviv37 <guyhavivcodes@gmail.com>
2022-03-18 13:33:48 +0200

    try netlify deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/nim.io-web">
+    <BrowserRouter>
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
